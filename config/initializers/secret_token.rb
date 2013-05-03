06a708d577c9161939cfb261dfc977e7aaa3f8ba
Eric Xu <e.xulander@gmail.com>
2013-05-03 10:58:11 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AsciiCastsDemo::Application.config.secret_token = '8e2f9e3945835186b21efd2bdde03da18dbfa7a19feaafd5ce3fdc777f2330e36c344a9e03f671671b5d054f98cace7eb292472843c36a3581ae2664a7a1211b'
