83b24b35a4f4a18168e5f858d1d08df2d6d70469
Eric Xu <e.xulander@gmail.com>
2013-05-07 23:48:15 -0700

    add additional episode controller actions and views

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -3,15 +3,42 @@ class EpisodesController < ApplicationController
 		@episode = Episode.new
 	end
 
+	def create
+		@episode = Episode.new(params[:episode])
+		if @episode.save
+			flash[:notice] = "Successfully created episode."
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
 	def index
 		@episodes = Episode.all
 	end
 
 	def edit
+		@episode = Episode.find(params[:id])
+	end
 
+	def update
+		@episode = Episode.find(params[:id])
+		if @episode.update_attributes(params[:episode])
+			flash[:notice] = "Successfully updated episode."
+			redirect_to root_path
+		else
+			render :edit
+		end
 	end
 
 	def destroy
-
+		# @episode = Episode.find(params[:id])
+		# @episode.destroy
+		# or
+		# Episode.find(params[:id]).destroy
+		# or
+		Episode.destroy(params[:id])
+		flash[:notice] = "Successfully deleted episode."
+		redirect_to root_path
 	end
 end
