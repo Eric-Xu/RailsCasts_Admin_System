9dcbd782763a029a5c702807fbeb4f728605a9d7
Eric Xu <e.xulander@gmail.com>
2013-05-08 00:28:03 -0700

    add admin? and authorize methods to application controller from ep20

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :admin?
+
+  protected
+  	def admin?
+  		true
+  	end
+
+  	def authorize
+  		unless admin?
+	  		flash[:error] = "Unauthorized access"
+	  		redirect_to root_path
+	  		false
+	  	end
+  	end
 end
--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,4 +1,6 @@
 class EpisodesController < ApplicationController
+	before_filter :authorize, :except => :index
+
 	def new
 		@episode = Episode.new
 	end
